1472cdca4190ec573ca36a7865563aa709542536
Ansh74284 <121850149+Ansh74284@users.noreply.github.com>
2024-05-14 01:35:44 +0530

    first part completed - backend

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+const generateTokenAndSetCookie = (userId, res) =>{
+    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
+        expiresIn: '365d',
+    });
+
+    res.cookie("jwt",token,{
+        maxAge: 365 * 24 * 60 * 60 * 1000, //MS
+        httpOnly: true, //prevent XSS attacks cross-site scripting attacks
+        sameSite: "strict", //CSRF Attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development",
+    });
+};
+
+export default generateTokenAndSetCookie;
