aa0d0862db45ec5dfb394ca6ea53fff004c89219
Ansh74284 <121850149+Ansh74284@users.noreply.github.com>
2024-05-18 13:26:52 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,14 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from './socket/socket.js';
 
-
-const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+
+
+
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser()); //to parse the incoming cookies (from req.cookies)
